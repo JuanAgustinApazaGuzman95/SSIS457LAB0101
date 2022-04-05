954037e0788a02c2e5077ad24629aca138278f23
JuanAgustinApazaGuzman95 <90398153+JuanAgustinApazaGuzman95@users.noreply.github.com>
2022-04-05 09:54:09 -0400

    si
    biografia de Juan Agustin Apaza Guzman

---
+++ SIS457LAB0102/principal.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "Soy Juan Agustin Apaza Guzman, tengo 20 años naci el 11 de julio de 2001 en la ciudad de La Paz, " << endl;
+	cout << "vivo en la ciudad de La Paz con mi mama y mi hermano menor, Estudio Ing. de sistemas en la USFX, " << endl;
+	cout << "me gusta la musica, aprender nuevas cosas cada dia, ver series, comer, nadar, voleibol, " << endl;
+	cout << "Mis metas son aprender a programar y poder trabajar siendo un muy buen programador. " << endl;
+	system("pause");
+	return 0;
+}
